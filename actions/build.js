9ac11b388feeca8ac946cf15d6bde7f77c8307cd
Thomas Jung <thomas.jung@sap.com>
2020-08-24 15:35:10 -0400

    parts debugging

--- actions/build.js
+++ actions/build.js
@@ -21,9 +21,8 @@ const main = async _ => {
 
         for (const item of list) {
             if (item.includes('https://github.com/')) {
-                console.log(item)
-
-                const parts = gh(item);
+                const parts = gh(item)
+                console.log(`${parts.owner}, ${parts.name}`)
                 let response = await octokit.request('GET /repos/{owner}/{repo}', {
                     owner: parts.owner,
                     repo: parts.name

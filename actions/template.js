a08e988dac6429a7899a717cffd065c1459746ca
Thomas Jung <thomas.jung@sap.com>
2020-08-24 16:46:46 -0400

    Owner HTML URL

--- actions/template.js
+++ actions/template.js
@@ -6,7 +6,7 @@ module.exports = `
 * Open Issues: {{data.open_issues_count}}, Has Projects: {{data.has_projects}}, Has Wiki: {{data.has_wiki}}
 * Created At: {{data.created_at}}, Updated At: {{data.updated_at}}
 * License: [{{data.license.name}}]({{data.license.url}})
-* Owner: [{{data.owner.login}}]({{data.owner.url}})
+* Owner: [{{data.owner.login}}]({{data.owner.html_url}})
 * Contributors: [{{data.contributors_url}}]({{data.contributors_url}})
 
 `

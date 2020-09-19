407db466f668df2c6fd19f82df50de6ee37ce5fb
Lars Hvam <larshp@hotmail.com>
2020-09-19 09:25:27 +0200

    entries.md: fix watchers

--- actions/template.js
+++ actions/template.js
@@ -2,7 +2,7 @@ module.exports = `
 ## {{data.name}}: {{data.description}}
 [{{data.html_url}}]({{data.html_url}})
 
-* Stars: {{data.stargazers_count}}, Forks: {{data.forks_count}}, Watchers: {{data.watchers_count}}
+* Stars: {{data.stargazers_count}}, Forks: {{data.forks_count}}, Watchers: {{data.subscribers_count}}
 * Open Issues: {{data.open_issues_count}}, Has Projects: {{data.has_projects}}, Has Wiki: {{data.has_wiki}}
 * Created At: {{data.created_at_formatted}}, Updated At: {{data.updated_at_formatted}}
 * License: {{#if data.license.html_url}}[{{data.license.name}}]({{data.license.html_url}}){{else}}{{data.license.name}}{{/if}}
